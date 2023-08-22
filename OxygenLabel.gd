bea22e5c0cefd75fe2260dbec54ee48da716be4b
BenevolentPista <pranav.sista@gmail.com>
2023-08-22 23:39:03 +0530

    Added code for how oxygen changes

---
+++ OxygenLabel.gd
@@ -0,0 +1,13 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	text = "Hi!"
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print("Hi")
+	text = "Oxygen: " + str(get_node("../..").oxygen)
+	pass
