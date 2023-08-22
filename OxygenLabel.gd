a23de565c8d8938dfa9c861b269a228a5ef52a6f
Sundeep Kumar Ammisetti <sammisetti2@gmail.com>
2023-08-22 13:48:39 -0500

    Changed Process Modes for Canvas Layer, Win Screen, and Oxygen Label
    - Changed Process mode values for 'Canvas Layer', ' WinScreen' and 'OxygenLabel' as the following: 'Always', 'When Paused' and 'Pausable'
    - Also changed oxygen label script name to lowercase for consistency

--- OxygenLabel.gd
+++ OxygenLabel.gd
@@ -8,6 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print("Hi")
 	text = "Oxygen: " + str(get_node("../..").oxygen)
-	pass
